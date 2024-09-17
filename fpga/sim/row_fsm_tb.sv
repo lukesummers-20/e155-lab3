56c4b08dee053140d84a55f133f3c0afc7c96462
Luke Summers <lsummers@g.hmc.edu>
2024-09-17 15:21:50 -0700

    updated row fsm tb

--- fpga/sim/row_fsm_tb.sv
+++ fpga/sim/row_fsm_tb.sv
@@ -1,11 +1,12 @@
 module row_fsm_tb();
     logic [3:0] row, row_expected;
-    logic en, clk, reset;
+    logic en, press, clk, reset;
 
-    row_fsm fsm1(en, clk, reset, row);
+    row_fsm fsm1(en, press, clk, reset, row);
 
     initial begin
         reset = 0;
+        press = 0;
         clk = 0; #5;
         row_expected = 4'b0001;
         en = 1; #5;
@@ -18,20 +19,35 @@ module row_fsm_tb();
         row_expected = 4'b0010;
         clk = 1; #5;
         if (row != row_expected) begin
-            $display(" row = %b row_expected = %b on first step", row, row_expected);
+            $display(" row = %b row_expected = %b on 1 step", row, row_expected);
         end
-	clk = 0; #5;
+	    clk = 0; #5;
         row_expected = 4'b0100;
         clk = 1; #5;
         if (row != row_expected) begin
-            $display(" row = %b row_expected = %b on first step", row, row_expected);
+            $display(" row = %b row_expected = %b on 2 step", row, row_expected);
         end
-	en = 0;
-	clk = 0; #5;
+	    en = 0;
+	    clk = 0; #5;
         row_expected = 4'b0100;
         clk = 1; #5;
         if (row != row_expected) begin
-            $display(" row = %b row_expected = %b on first step", row, row_expected);
+            $display(" row = %b row_expected = %b on 3 step", row, row_expected);
+        end
+        clk = 0;
+        en = 1; #5;
+        row_expected = 4'b1000;
+        clk = 1; #5;
+        if (row != row_expected) begin
+            $display(" row = %b row_expected = %b on 4 step", row, row_expected);
+        end
+        clk = 0;
+        en = 0;
+        press = 1; #5;
+        row_expected = 4'b0100;
+        clk = 1; #5;
+        if (row != row_expected) begin
+            $display(" row = %b row_expected = %b on 5 step", row, row_expected);
         end
     end
 endmodule
