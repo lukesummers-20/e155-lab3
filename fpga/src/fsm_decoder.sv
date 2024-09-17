9a89f7420a2b5981adf0b53bf596714ae25ee120
Luke Summers <lsummers@g.hmc.edu>
2024-09-17 15:11:17 -0700

    updated decoder

--- fpga/src/fsm_decoder.sv
+++ fpga/src/fsm_decoder.sv
@@ -1,13 +1,21 @@
 module fsm_decoder(
     input  logic [3:0] col, row,
-    input  logic en,
+    input  logic en, clk, reset,
     output logic [3:0] s1, s2,
 );
     logic [3:0] next;
     // update signals on en
-    always_ff @(posedge en) begin
-        s2 <= s1;
-        s1 <= next;
+    always_ff @(posedge clk) begin
+        if (reset == 0) begin 
+            s1 <= 4'b0000;
+            s2 <= 4'b0000;
+        end else if (en) begin 
+            s2 <= s1;
+            s1 <= next;
+        end else begin
+            s1 <= s1;
+            s2 <= s2;
+        end
     end
     // logic for next signal
     always_comb
@@ -41,10 +49,10 @@ module fsm_decoder(
                      end
             4'b1000: begin
                         case(col)
-                        4'b0001: next = 4'b1110;
-                        4'b0010: next = 4'b0000;
-                        4'b0100: next = 4'b1111;
-                        4'b1000: next = 4'b1101;
+                            4'b0001: next = 4'b1110;
+                            4'b0010: next = 4'b0000;
+                            4'b0100: next = 4'b1111;
+                            4'b1000: next = 4'b1101;
                             default: next = 4'b0000;
                         endcase
                      end
